58bdf416a1059cc3b4383ffecf8fef4d41e03c5a
utkarshbankar <utkar7hbankar@gmail.com>
2022-07-20 13:55:47 +0530

    Project structure added.

---
+++ projects/resto/src/app/restohome/restohome.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restohome',
+  templateUrl: './restohome.component.html',
+  styleUrls: ['./restohome.component.scss']
+})
+export class RestohomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
