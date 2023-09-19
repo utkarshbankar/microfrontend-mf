bae38e571462e0d1b9aeda4c0187d04fa7e238c4
utkarshbankar <utkar7hbankar@gmail.com>
2023-09-19 20:07:19 +0530

    created new branch for custom webpack configs

--- projects/shell/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { loadManifest } from '@angular-architects/module-federation';
-
-loadManifest("/assets/mf.manifest.json")
-  .catch(err => console.error(err))
-  .then(_ => import('./bootstrap'))
-  .catch(err => console.error(err));
