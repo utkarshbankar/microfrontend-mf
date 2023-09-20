a352376083c55abecb3e3a8a98f85da8250ce4f6
utkarshbankar <utkar7hbankar@gmail.com>
2023-09-20 23:04:17 +0530

    updated webpack config files

--- projects/mf-remote/webpack.config.js
+++ projects/mf-remote/webpack.config.js
@@ -1,1 +1,29 @@
 
+const webpack = require('webpack');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+
+module.exports = {
+  output: {
+    publicPath: 'http://localhost:4201/',
+    uniqueName: 'mfremote',
+    scriptType: 'text/javascript',
+  },
+  optimization: {
+    runtimeChunk: false,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'profile',
+      library: { type: 'var', name: 'profile' },
+      filename: 'remoteEntry.js',
+      exposes: {
+        ProfileModule: './projects/mf-remote/src/app/app.module.ts',
+      },
+      shared: {
+        '@angular/core': { singleton: true, eager: true },
+        '@angular/common': { singleton: true, eager: true },
+        '@angular/router': { singleton: true, eager: true }
+      },
+    }),
+  ],
+};
