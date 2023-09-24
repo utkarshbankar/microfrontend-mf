c29de8bd1209e38cc816cfcb84c2a39f67a47d6a
utkarshbankar <utkar7hbankar@gmail.com>
2023-09-24 17:01:13 +0530

    loading the remote app in side shell dynamically or without routes

--- projects/mf-shell/src/app/home/home.component.ts
+++ projects/mf-shell/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, Injector, NgModuleRef, ViewChild, ViewContainerRef, createNgModule } from '@angular/core';
+import { Component, Injector, NgModuleRef, ViewChild, ViewContainerRef, createNgModule, OnInit } from '@angular/core';
 import { loadRemoteModule } from '../utils/federation-utils';
 
 @Component({
@@ -6,6 +6,20 @@ import { loadRemoteModule } from '../utils/federation-utils';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  @ViewChild('moduleLoader', {read: ViewContainerRef}) remoteModule!: ViewContainerRef;
+  viewContainer: any;
+
+ constructor(private injector:Injector){}
+  async ngOnInit() {
+    const m = await loadRemoteModule({
+      remoteName: 'dynamicRemoteApp' ,
+      remoteEntry: 'http://localhost:4400/dynamicRemoteAppEntry.js',
+      exposedModule: 'dynamicRemoteAppModule'
+    }) ;
+    const modRef:NgModuleRef<any> = createNgModule(m.LazyDynamicRemoteModule , this.injector);
+    const moduleFactory = modRef.instance.resolveComponent();
+    this.remoteModule.createComponent(moduleFactory, {ngModuleRef: modRef});
+   }
 }
