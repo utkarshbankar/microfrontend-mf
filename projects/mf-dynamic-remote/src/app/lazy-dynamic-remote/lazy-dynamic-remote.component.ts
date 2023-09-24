de06896062570d21ba4a626c9a62b6093538f0f7
utkarshbankar <utkar7hbankar@gmail.com>
2023-09-24 17:16:43 +0530

    added support to pass input prop to the dynamic loading module through instance prop

--- projects/mf-dynamic-remote/src/app/lazy-dynamic-remote/lazy-dynamic-remote.component.ts
+++ projects/mf-dynamic-remote/src/app/lazy-dynamic-remote/lazy-dynamic-remote.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-lazy-dynamic-remote',
   templateUrl: './lazy-dynamic-remote.component.html',
   styleUrls: ['./lazy-dynamic-remote.component.scss']
 })
-export class LazyDynamicRemoteComponent {
-
+export class LazyDynamicRemoteComponent implements OnInit {
+  @Input() inputFromShell:any;
+  inputProp:any;
+  ngOnInit(){
+    console.log("inside the dynamic remote", this.inputFromShell);
+     
+  }
 }
