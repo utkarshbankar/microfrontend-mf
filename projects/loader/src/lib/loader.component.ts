4223ca2c057d2186b33116e1dd43714aa9e4a776
utkarshbankar <utkar7hbankar@gmail.com>
2023-09-29 22:40:17 +0530

    added loader lib for loading scenario

---
+++ projects/loader/src/lib/loader.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-loader',
+  template: `
+    <p>
+      loader works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class LoaderComponent {
+
+}
